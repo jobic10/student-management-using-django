ed6ddb3577dceac9da9a55bd3d272c0e127443d8
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-10-07 21:18:10 +0100

    Student Dashboard Fixed

--- main_app/student_views.py
+++ main_app/student_views.py
@@ -1,6 +1,6 @@
 import json
 from datetime import datetime
-
+import math
 from django.contrib import messages
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse, JsonResponse
@@ -14,10 +14,44 @@ from .models import *
 
 
 def student_home(request):
-    return render(request, 'student_template/home_content.html')
+    student = get_object_or_404(Student, admin=request.user)
+    total_subject = Subject.objects.filter(course=student.course).count()
+    total_attendance = AttendanceReport.objects.filter(student=student).count()
+    total_present = AttendanceReport.objects.filter(
+        student=student, status=True).count()
+    if total_attendance == 0:  # Don't divide. DivisionByZero
+        percent_absent, percent_present = 0
+    else:
+        percent_present = math.floor((total_present/total_attendance) * 100)
+        percent_absent = math.ceil(100 - percent_present)
+    subject_name = []
+    data_present = []
+    data_absent = []
+    subjects = Subject.objects.filter(course=student.course)
+    for subject in subjects:
+        attendance = Attendance.objects.filter(subject=subject)
+        present_count = AttendanceReport.objects.filter(
+            attendance__in=attendance, status=True, student=student).count()
+        absent_count = AttendanceReport.objects.filter(
+            attendance__in=attendance, status=False, student=student).count()
+        subject_name.append(subject.name)
+        data_present.append(present_count)
+        data_absent.append(absent_count)
+    context = {
+        'total_attendance': total_attendance,
+        'percent_present': percent_present,
+        'percent_absent': percent_absent,
+        'total_subject': total_subject,
+        'subjects': subjects,
+        'data_present': data_present,
+        'data_absent': data_absent,
+        'data_name': subject_name
+
+    }
+    return render(request, 'student_template/home_content.html', context)
 
 
-@csrf_exempt
+@ csrf_exempt
 def student_view_attendance(request):
     student = get_object_or_404(Student, admin=request.user)
     if request.method != 'POST':
