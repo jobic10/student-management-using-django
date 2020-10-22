bbeeb00e74d26bc322dc29b4823d137e5f36ac56
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-10-22 15:21:51 +0100

    Code description

--- main_app/LoginCheckMiddleWare.py
+++ main_app/LoginCheckMiddleWare.py
@@ -6,18 +6,18 @@ from django.shortcuts import redirect
 class LoginCheckMiddleWare(MiddlewareMixin):
     def process_view(self, request, view_func, view_args, view_kwargs):
         modulename = view_func.__module__
-        user = request.user
+        user = request.user # Who is the current user ?
         if user.is_authenticated:
-            if user.user_type == '1':
+            if user.user_type == '1': # Is it the HOD/Admin
                 if modulename == 'main_app.student_views':
                     return redirect(reverse('admin_home'))
-            elif user.user_type == '2':
+            elif user.user_type == '2': #  Staff :-/ ?
                 if modulename == 'main_app.student_views' or modulename == 'main_app.hod_views':
                     return redirect(reverse('staff_home'))
-            elif user.user_type == '3':
+            elif user.user_type == '3': # ... or Student ?
                 if modulename == 'main_app.hod_views' or modulename == 'main_app.staff_views':
                     return redirect(reverse('student_home'))
-            else:
+            else: # None of the aforementioned ? Please take the user to login page
                 return redirect(reverse('login_page'))
         else:
             if request.path == reverse('login_page') or modulename == 'django.contrib.auth.views' or request.path == reverse('user_login'):
