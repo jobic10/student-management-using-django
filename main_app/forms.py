b83a259b2182c924b39a1a1174a1ba9d2ddd1040
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-10-27 16:20:14 +0100

    Patch

--- main_app/forms.py
+++ main_app/forms.py
@@ -8,8 +8,8 @@ class FormSettings(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(FormSettings, self).__init__(*args, **kwargs)
         # Here make some changes such as:
-        for visible in self.visible_fields():
-            visible.field.widget.attrs['class'] = 'form-control'
+        for field in self.visible_fields():
+            field.field.widget.attrs['class'] = 'form-control'
 
 
 class CustomUserForm(FormSettings):
